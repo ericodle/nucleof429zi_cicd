a3d7e9c510175972e513a55b65c7f4886f66e00d
EO <504068014080@tutanota.com>
2025-02-20 09:35:01 +0900

    main.c

--- app/src/main.c
+++ app/src/main.c
@@ -1,80 +1,57 @@
-/*
- * Copyright (c) 2021 Nordic Semiconductor ASA
- * SPDX-License-Identifier: Apache-2.0
- */
-
+#include <stdio.h>
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/sensor.h>
-#include <zephyr/logging/log.h>
+#include <zephyr/drivers/gpio.h>
 
-#include <app/drivers/blink.h>
+#define DOT_TIME_MS 200   // Duration of a dot
+#define DASH_TIME_MS 600  // Duration of a dash (3x dot time)
+#define GAP_TIME_MS 200   // Gap between elements
+#define LETTER_GAP_MS 600 // Gap between letters
+#define WORD_GAP_MS 1400  // Gap between words
 
-#include <app_version.h>
+#define LED0_NODE DT_ALIAS(led0)
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
-LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
-
-#define BLINK_PERIOD_MS_STEP 100U
-#define BLINK_PERIOD_MS_MAX  1000U
-
-int main(void)
-{
-	int ret;
-	unsigned int period_ms = BLINK_PERIOD_MS_MAX;
-	const struct device *sensor, *blink;
-	struct sensor_value last_val = { 0 }, val;
-
-	printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
-
-	sensor = DEVICE_DT_GET(DT_NODELABEL(example_sensor));
-	if (!device_is_ready(sensor)) {
-		LOG_ERR("Sensor not ready");
-		return 0;
-	}
-
-	blink = DEVICE_DT_GET(DT_NODELABEL(blink_led));
-	if (!device_is_ready(blink)) {
-		LOG_ERR("Blink LED not ready");
-		return 0;
-	}
-
-	ret = blink_off(blink);
-	if (ret < 0) {
-		LOG_ERR("Could not turn off LED (%d)", ret);
-		return 0;
-	}
-
-	printk("Use the sensor to change LED blinking period\n");
-
-	while (1) {
-		ret = sensor_sample_fetch(sensor);
-		if (ret < 0) {
-			LOG_ERR("Could not fetch sample (%d)", ret);
-			return 0;
-		}
-
-		ret = sensor_channel_get(sensor, SENSOR_CHAN_PROX, &val);
-		if (ret < 0) {
-			LOG_ERR("Could not get sample (%d)", ret);
-			return 0;
-		}
-
-		if ((last_val.val1 == 0) && (val.val1 == 1)) {
-			if (period_ms == 0U) {
-				period_ms = BLINK_PERIOD_MS_MAX;
-			} else {
-				period_ms -= BLINK_PERIOD_MS_STEP;
-			}
-
-			printk("Proximity detected, setting LED period to %u ms\n",
-			       period_ms);
-			blink_set_period_ms(blink, period_ms);
-		}
-
-		last_val = val;
-
-		k_sleep(K_MSEC(100));
-	}
-
-	return 0;
+void blink_led(int duration) {
+    gpio_pin_set_dt(&led, 1);
+    k_msleep(duration);
+    gpio_pin_set_dt(&led, 0);
+    k_msleep(GAP_TIME_MS);
 }
 
+void blink_sos() {
+    // SOS: ... --- ...
+    int sos_pattern[] = {DOT_TIME_MS, DOT_TIME_MS, DOT_TIME_MS,  
+                         DASH_TIME_MS, DASH_TIME_MS, DASH_TIME_MS,
+                         DOT_TIME_MS, DOT_TIME_MS, DOT_TIME_MS};
+    
+    for (int i = 0; i < 9; i++) {
+        blink_led(sos_pattern[i]);
+        if (i == 2 || i == 5) {
+            k_msleep(LETTER_GAP_MS);  // Longer gap between letters
+        }
+    }
+    k_msleep(WORD_GAP_MS);  // Pause before repeating
+}
+
+int main(void) {
+    int ret;
+    
+    printf("Your app for the NUCLEO-F429ZI is working!\n");
+
+    if (!gpio_is_ready_dt(&led)) {
+        printf("Error: GPIO device is not ready\n");
+        return 0;
+    }
+
+    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    if (ret < 0) {
+        printf("Error: Failed to configure LED pin\n");
+        return 0;
+    }
+
+    while (1) {
+        blink_sos();
+    }
+
+    return 0;
+}
