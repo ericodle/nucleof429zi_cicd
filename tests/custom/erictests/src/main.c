351f218133fde731d4f61df5fc247e485f71358e
EO <504068014080@tutanota.com>
2025-02-22 05:16:32 -0800

    revert

--- tests/custom/erictests/src/main.c
+++ tests/custom/erictests/src/main.c
@@ -2,6 +2,7 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 
+/* Reproduced macros and functions from actual app /src/main.c */
 #define DOT_TIME_MS 200
 #define DASH_TIME_MS 600
 #define GAP_TIME_MS 200  
@@ -11,39 +12,15 @@
 #define LED0_NODE DT_ALIAS(led0)
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
-struct blink_fixture {
-    int mock_gpio_calls;
-    int mock_sleep_calls;
-};
-
-static void *blink_setup(void)
-{
-    struct blink_fixture *fixture = malloc(sizeof(struct blink_fixture));
-    zassume_not_null(fixture, "Fixture allocation failed");
-    return fixture;
-}
-
-static void blink_before(void *f)
-{
-    struct blink_fixture *fixture = (struct blink_fixture *)f;
-    fixture->mock_gpio_calls = 0;
-    fixture->mock_sleep_calls = 0;
-}
-
-/* Mock functions */
-static int mock_gpio_pin_set_dt_call_count = 0;
-static int mock_k_msleep_call_count = 0;
-
-void blink_led(int duration)
-{
-    mock_gpio_pin_set_dt_call_count++;
+void blink_led(int duration) {
+    gpio_pin_set_dt(&led, 1);
     k_msleep(duration);
-    mock_gpio_pin_set_dt_call_count++;
+    gpio_pin_set_dt(&led, 0);
     k_msleep(GAP_TIME_MS);
 }
 
-void blink_sos()
-{
+void blink_sos() {
+    // SOS: ... --- ...
     int sos_pattern[] = {DOT_TIME_MS, DOT_TIME_MS, DOT_TIME_MS,  
                          DASH_TIME_MS, DASH_TIME_MS, DASH_TIME_MS,
                          DOT_TIME_MS, DOT_TIME_MS, DOT_TIME_MS};
@@ -57,27 +34,34 @@ void blink_sos()
     k_msleep(WORD_GAP_MS);
 }
 
-/* Test if blink_led correctly sets the GPIO pin */
-ZTEST_F(blink_led_tests, test_blink_led)
-{
-    blink_led(DOT_TIME_MS);
+/* Mock functions for gpio_pin_set_dt and k_msleep */
+static int mock_gpio_pin_set_dt_call_count = 0;
+static int mock_k_msleep_call_count = 0;
 
+/* Test if blink_led correctly sets the GPIO pin */
+ZTEST(blink_led_tests, test_blink_led) {
+    int duration = 200;
+
+    /* Call function to test */
+    blink_led(duration);
+
+    /* Assert that mock_gpio_pin_set_dt was called at least once */
     zassert_true(mock_gpio_pin_set_dt_call_count > 0, "GPIO set was not called");
+
+    /* You could also assert the number of times k_msleep is called, depending on the logic of blink_led */
     zassert_true(mock_k_msleep_call_count > 0, "Sleep function was not called");
 }
 
 /* Test if blink_sos correctly executes the SOS pattern */
-ZTEST_F(blink_led_tests, test_blink_sos)
-{
+ZTEST(blink_led_tests, test_blink_sos) {
     blink_sos();
 
+    /* Assert that mock_gpio_pin_set_dt was called multiple times */
     zassert_true(mock_gpio_pin_set_dt_call_count > 5, "GPIO set was not called enough times for SOS");
+
+    /* Assert that k_msleep was called the correct number of times */
     zassert_true(mock_k_msleep_call_count > 0, "Sleep function was not called during SOS sequence");
 }
 
-static void blink_teardown(void *f)
-{
-    free(f);
-}
-
-ZTEST_SUITE(blink_led_tests, NULL, blink_setup, blink_before, NULL, blink_teardown);
+/* Test Suite Registration */
+ZTEST_SUITE(blink_led_tests, NULL, NULL, NULL, NULL, NULL);
