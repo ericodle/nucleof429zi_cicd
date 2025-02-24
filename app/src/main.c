5261473eee5dbaad5be24b6f9651d21b84e4ee38
EO <504068014080@tutanota.com>
2025-02-24 14:54:49 +0900

    mores refactor

--- app/src/main.c
+++ app/src/main.c
@@ -1,37 +1,6 @@
+// main.c
 #include <stdio.h>
-#include <zephyr/kernel.h>
-#include <zephyr/drivers/gpio.h>
-
-#define DOT_TIME_MS 200   // Duration of a dot
-#define DASH_TIME_MS 600  // Duration of a dash (3x dot time)
-#define GAP_TIME_MS 200   // Gap between elements
-#define LETTER_GAP_MS 600 // Gap between letters
-#define WORD_GAP_MS 1400  // Gap between words
-
-#define LED0_NODE DT_ALIAS(led0)
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-
-void blink_led(int duration) {
-    gpio_pin_set_dt(&led, 1);
-    k_msleep(duration);
-    gpio_pin_set_dt(&led, 0);
-    k_msleep(GAP_TIME_MS);
-}
-
-void blink_sos() {
-    // SOS: ... --- ...
-    int sos_pattern[] = {DOT_TIME_MS, DOT_TIME_MS, DOT_TIME_MS,  
-                         DASH_TIME_MS, DASH_TIME_MS, DASH_TIME_MS,
-                         DOT_TIME_MS, DOT_TIME_MS, DOT_TIME_MS};
-    
-    for (int i = 0; i < 9; i++) {
-        blink_led(sos_pattern[i]);
-        if (i == 2 || i == 5) {
-            k_msleep(LETTER_GAP_MS);  // Longer gap between letters
-        }
-    }
-    k_msleep(WORD_GAP_MS);  // Pause before repeating
-}
+#include "morse.h"
 
 int main(void) {
     int ret;
---
+++ app/src/morse.c
@@ -0,0 +1,26 @@
+// morse.c
+#include "morse.h"
+
+#define LED0_NODE DT_ALIAS(led0)
+const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+void blink_led(int duration) {
+    gpio_pin_set_dt(&led, 1);
+    k_msleep(duration);
+    gpio_pin_set_dt(&led, 0);
+    k_msleep(GAP_TIME_MS);
+}
+
+void blink_sos() {
+    int sos_pattern[] = {DOT_TIME_MS, DOT_TIME_MS, DOT_TIME_MS,  
+                         DASH_TIME_MS, DASH_TIME_MS, DASH_TIME_MS,
+                         DOT_TIME_MS, DOT_TIME_MS, DOT_TIME_MS};
+    
+    for (int i = 0; i < 9; i++) {
+        blink_led(sos_pattern[i]);
+        if (i == 2 || i == 5) {
+            k_msleep(LETTER_GAP_MS);
+        }
+    }
+    k_msleep(WORD_GAP_MS);
+}
