e499cce10c8fbecb6d2b50ea35d4d596006d4df9
EO <504068014080@tutanota.com>
2025-02-24 12:43:31 +0900

    Revert "refactor morse"
    This reverts commit 885ac3f707717a945e6151b56c6bf9e2aa07279a.

--- app/src/main.c
+++ app/src/main.c
@@ -1,8 +1,37 @@
-// main.c
 #include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
-#include "morse.h"  // Include the morse functions header
+
+#define DOT_TIME_MS 200   // Duration of a dot
+#define DASH_TIME_MS 600  // Duration of a dash (3x dot time)
+#define GAP_TIME_MS 200   // Gap between elements
+#define LETTER_GAP_MS 600 // Gap between letters
+#define WORD_GAP_MS 1400  // Gap between words
+
+#define LED0_NODE DT_ALIAS(led0)
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+void blink_led(int duration) {
+    gpio_pin_set_dt(&led, 1);
+    k_msleep(duration);
+    gpio_pin_set_dt(&led, 0);
+    k_msleep(GAP_TIME_MS);
+}
+
+void blink_sos() {
+    // SOS: ... --- ...
+    int sos_pattern[] = {DOT_TIME_MS, DOT_TIME_MS, DOT_TIME_MS,  
+                         DASH_TIME_MS, DASH_TIME_MS, DASH_TIME_MS,
+                         DOT_TIME_MS, DOT_TIME_MS, DOT_TIME_MS};
+    
+    for (int i = 0; i < 9; i++) {
+        blink_led(sos_pattern[i]);
+        if (i == 2 || i == 5) {
+            k_msleep(LETTER_GAP_MS);  // Longer gap between letters
+        }
+    }
+    k_msleep(WORD_GAP_MS);  // Pause before repeating
+}
 
 int main(void) {
     int ret;
@@ -21,7 +50,7 @@ int main(void) {
     }
 
     while (1) {
-        blink_sos();  // Call the SOS blinking function
+        blink_sos();
     }
 
     return 0;
