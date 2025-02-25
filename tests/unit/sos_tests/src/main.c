bc14de6aa5b68cb9bca45de9991ed941f5dec7a9
EO <504068014080@tutanota.com>
2025-02-25 11:27:06 +0900

    print test

--- tests/unit/sos_tests/src/main.c
+++ tests/unit/sos_tests/src/main.c
@@ -1,26 +1,10 @@
-/*
-TODO: write actual unit tests for SOS.
- */
+#include <zephyr/ztest.h>
+#include <stdio.h>
 
+ZTEST(app_test, test_printf)
+{
+    printk("Your app for the NUCLEO-F429ZI is working!\n");
+    zassert_true(1, "Printf test passed!");
+}
 
-
- #include <zephyr/ztest.h>
-
-
- ZTEST_SUITE(framework_tests, NULL, NULL, NULL, NULL, NULL);
- 
- /**
-  * @brief Test Asserts
-  *
-  * This test verifies various assert macros provided by ztest.
-  *
-  */
- ZTEST(framework_tests, test_assert)
- {
-     zassert_true(1, "1 was false");
-     zassert_false(0, "0 was true");
-     zassert_is_null(NULL, "NULL was not NULL");
-     zassert_not_null("foo", "\"foo\" was NULL");
-     zassert_equal(1, 1, "1 was not equal to 1");
-     zassert_equal_ptr(NULL, NULL, "NULL was not equal to NULL");
- }
+ZTEST_SUITE(app_test, NULL, NULL, NULL, NULL, NULL);
