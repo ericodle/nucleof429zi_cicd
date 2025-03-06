f8916f2737442b9cd4e0de67d1983e980512c4fb
EO <504068014080@tutanota.com>
2025-03-07 08:57:39 +0900

    multi-os setup

---
+++ test-containers/arch/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e  # Exit immediately if a command fails
+
+# Activate the virtual environment
+source /opt/venv/bin/activate
+
+# Initialize the West workspace only if it hasn't been initialized yet
+if [ ! -d "debian-test" ]; then
+    west init -m https://github.com/ericodle/nucleof429zi_cicd --mr main debian-test
+fi
+
+# Update West
+cd debian-test
+west update
+
+# Install required Python packages
+pip install --no-cache-dir natsort pyelftools junitparser pytest psutil tabulate pyserial anytree ply
+
+# Move into the test directory
+cd nucleof429zi_cicd
+
+# Run the tests
+west twister -T tests --verbose --integration
+
+# Keep the container running (optional)
+exec "$@"
